df0d89b2e33e97005f9c68eba548315855e46d2c
kevlartest <68110443+kevlartest@users.noreply.github.com>
2020-07-17 15:07:34 +0100

    Add slideshow to gallery page (#4)

---
+++ portfolio/src/main/webapp/gallery.js
@@ -0,0 +1,47 @@
+let slideIndex = 0;
+let slides = document.getElementsByClassName("slides");
+let indicators = document.getElementsByClassName("indicator");
+
+function displaySlide(n) {
+    // Loop around if we reach the last slide
+    slideIndex = (n + slides.length) % slides.length;
+
+    Array.from(slides).forEach(slide => slide.style.display = "none");
+    Array.from(indicators).forEach(indicator => indicator.className = indicator.className.replace(" active", ""));
+
+    slides[slideIndex].style.display = "block";
+    indicators[slideIndex].className += " active";
+}
+
+function nextSlide() {
+  displaySlide(++slideIndex);
+}
+
+function previousSlide() {
+  displaySlide(--slideIndex);
+}
+
+function setSlide(n) {
+  displaySlide(slideIndex = n);
+}
+
+// Allow interacting with arrow keys
+document.addEventListener('keydown', function(event) {
+    switch (event.key) {
+        case "ArrowLeft":
+        previousSlide();
+        break;
+
+        case "ArrowRight":
+        nextSlide();
+        break;
+
+        case "Home": case "PageUp":
+        setSlide(0);
+        break;
+
+        case "End": case "PageDown":
+        setSlide(slides.length - 1);
+        break;
+  }
+});
