614cfc1e32948bc76c9b8f936208166afe01dabe
kevlartest <kevlartest@google.com>
2020-07-20 14:28:59 +0000

    Add comment form

--- portfolio/src/main/java/com/google/sps/servlets/CommentsServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/CommentsServlet.java
@@ -15,18 +15,35 @@
 package com.google.sps.servlets;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/** Servlet that returns some example content. TODO: modify this file to handle comments data */
-@WebServlet("/data")
-public class DataServlet extends HttpServlet {
+@WebServlet("/comments")
+public class CommentsServlet extends HttpServlet {
 
-  @Override
-  public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    response.setContentType("text/html;");
-    response.getWriter().println("<h1>Hello world!</h1>");
-  }
+  private Map<String,String> comments;
+
+    @Override
+    public void init() {
+        comments = new HashMap<>();
+    }
+
+    @Override
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
+        String email = request.getParameter("email");
+        String body = request.getParameter("body");
+
+        System.out.println("Email: " + email + " Comment: " + body);
+
+        comments.put(email,body);
+
+        response.sendRedirect("/index.html");
+    }
 }
