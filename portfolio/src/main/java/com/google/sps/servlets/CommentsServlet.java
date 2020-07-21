0be7f8efd0fb8c50b9da2a8bbbbee03a1bee0a19
kevlartest <68110443+kevlartest@users.noreply.github.com>
2020-07-21 17:08:46 +0100

    Add Comment form (#8)

---
+++ portfolio/src/main/java/com/google/sps/servlets/CommentsServlet.java
@@ -0,0 +1,58 @@
+// Copyright 2019 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.sps.servlets;
+
+import com.google.sps.data.Comment;
+import java.io.IOException;
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/comments")
+public class CommentsServlet extends HttpServlet {
+
+  private Map<String,List<Comment>> comments;
+
+    @Override
+    public void init() {
+        comments = new HashMap<>();
+    }
+
+    @Override
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
+        final String email = request.getParameter("email");
+        final String body = request.getParameter("body");
+        final Instant timestamp = Instant.now();
+
+        final Comment comment = new Comment(email,body,timestamp);
+
+        System.out.println("Timestamp: " + timestamp + " Email: " + email + " Comment: " + body);
+
+        if(!comments.containsKey(email)){
+            comments.put(email, new ArrayList<>());
+        }
+
+        comments.get(email).add(comment);
+
+        response.sendRedirect("/index.html");
+    }
+}
