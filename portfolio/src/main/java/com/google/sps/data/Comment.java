451620911b9580e5de2bb56a6a7903e4f1e12ab1
kevlartest <kevlartest@google.com>
2020-07-21 12:23:54 +0000

    Allow permanently storing comments

--- portfolio/src/main/java/com/google/sps/data/Comment.java
+++ portfolio/src/main/java/com/google/sps/data/Comment.java
@@ -2,6 +2,7 @@ package com.google.sps.data;
 
 import java.time.Instant;
 import java.time.temporal.ChronoUnit;
+import com.google.appengine.api.datastore.Entity;
 
 public class Comment {
     private final String email, body;
@@ -25,5 +26,14 @@ public class Comment {
 
     public String getFormattedTimestamp(){
         return timestamp.truncatedTo(ChronoUnit.SECONDS).toString();
+    }
+
+    public Entity toDatastoreEntity(){
+        Entity entity = new Entity("Comment");
+        entity.setProperty("email", email);
+        entity.setProperty("body", body);
+        entity.setProperty("timestamp", timestamp.toString());
+
+        return entity;
     }
 }
