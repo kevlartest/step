00bc793efbf77e018b529651091422ca210cb91e
kevlartest <kevlartest@google.com>
2020-07-22 10:59:33 +0000

    Style the comments

--- portfolio/src/main/webapp/comments.js
+++ portfolio/src/main/webapp/comments.js
@@ -18,15 +18,24 @@ function createCommentElement(comment) {
     const options = { hour: 'numeric', minute: 'numeric', weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
     const formattedTimestamp = timestamp.toLocaleDateString('en-IE', options);
 
-    const emailElement = document.createElement('p');
+    const commentHeading = document.createElement('p');
+
+    const emailElement = document.createElement('span');
     emailElement.className = 'commentEmail';
-    emailElement.innerText = comment.email + " (" + formattedTimestamp + "):";
+    emailElement.innerText = comment.email;
+
+    const timestampElement = document.createElement('span');
+    timestampElement.className = 'commentTimestamp';
+    timestampElement.innerText = formattedTimestamp;
+
+    commentHeading.appendChild(emailElement);
+    commentHeading.appendChild(timestampElement);
 
     const bodyElement = document.createElement('p');
     bodyElement.className = 'commentBody';
     bodyElement.innerText = comment.body;
 
-    commentElement.appendChild(emailElement);
+    commentElement.appendChild(commentHeading);
     commentElement.appendChild(bodyElement);
 
     return commentElement;
