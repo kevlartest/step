785528d25d5baccfca532eb13e9374891695eba6
kevlartest <kevlartest@google.com>
2020-08-05 11:20:36 +0100

    Simplify user admin check logic

--- portfolio/src/main/java/com/google/sps/servlets/LoginDataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/LoginDataServlet.java
@@ -3,13 +3,12 @@ package com.google.sps.servlets;
 import com.google.appengine.api.users.UserService;
 import com.google.appengine.api.users.UserServiceFactory;
 import com.google.sps.data.LoginData;
-import java.io.IOException;
-import java.io.PrintWriter;
+
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.ServletException;
+import java.io.IOException;
 
 @WebServlet("/logindata")
 public class LoginDataServlet extends HttpServlet {
@@ -21,7 +20,7 @@ public class LoginDataServlet extends HttpServlet {
     UserService userService = UserServiceFactory.getUserService();
 
     final boolean loggedIn = userService.isUserLoggedIn();
-    final boolean isUserAdmin = loggedIn ? userService.isUserAdmin() : false;
+    final boolean isUserAdmin = loggedIn && userService.isUserAdmin();
     final String userId = loggedIn ? userService.getCurrentUser().getUserId() : "";
 
     final LoginData loginData = new LoginData(loggedIn,userId,isUserAdmin);
