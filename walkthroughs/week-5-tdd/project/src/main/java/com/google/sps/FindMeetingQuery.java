9e9b3e82ecdfed67fecbf7afda1b6b192b40b536
kevlartest <kevlartest@google.com>
2020-08-10 10:16:39 +0100

    Do not use wildcard imports

--- walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
+++ walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
@@ -14,7 +14,14 @@
 
 package com.google.sps;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeSet;
 
 public final class FindMeetingQuery {
 
